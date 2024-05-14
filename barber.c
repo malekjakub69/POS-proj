9d325c7029a831a79a5385ec0cc35ea8b36de36e
Jacob Malek <malekjakub69@gmail.com>
2024-05-14 15:54:17 +0200

    commit

--- barber.c
+++ barber.c
@@ -10,6 +10,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/queue.h>
 
 #define CUSTOMERS 50
 #define CHAIRS 5
@@ -21,25 +22,33 @@
 #define true 1
 #define false 0
 
-#define print true
-// Values for the barber and customers synchronization
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t barber_available = PTHREAD_COND_INITIALIZER;
-pthread_cond_t customer_waiting = PTHREAD_COND_INITIALIZER;
-
-int waiting = 0; // Number of customers waiting
-int is_barber_sleeping = true;
-
 // Prototype of functions
 void *barber(void *arg);
 void *customer(void *arg);
 void barber_sleep();
-void barber_service();
+void barber_service(int customer_number);
 void customer_entry(int number);
 void customer_wait(int number);
-void customer_service(int number);
+void customer_served(int number);
 void customer_exit(int number);
 void customer_reject(int number);
+
+// Values for the barber and customers synchronization
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t barber_cond = PTHREAD_COND_INITIALIZER;
+int customers_waiting = 0;
+
+// Define the structure of the queue
+typedef struct entry
+{
+    int id;
+    pthread_cond_t cond;
+    TAILQ_ENTRY(entry)
+    entries;
+} entry;
+
+TAILQ_HEAD(tailhead, entry)
+head = TAILQ_HEAD_INITIALIZER(head);
 
 int served_customers = 0;
 int rejected_customers = 0;
@@ -47,16 +56,18 @@ int barber_cutting = 0;
 
 int main()
 {
-    pthread_t barber_thread, customer_threads[CUSTOMERS];
     srand((unsigned)time(NULL));
+
+    pthread_t barber_thread, customer_threads[CUSTOMERS];
+    pthread_cond_init(&barber_cond, NULL);
 
     // Create barber thread
     pthread_create(&barber_thread, NULL, barber, NULL);
 
     // Simulate the arrival of customers
-    for (int i = 0; i < CUSTOMERS; i++)
+    for (long i = 0; i < CUSTOMERS; i++)
     {
-        pthread_create(&customer_threads[i], NULL, customer, (void *)(long)i);
+        pthread_create(&customer_threads[i], NULL, customer, (void *)i);
         int interval = (rand() % (MAX_INTERVAL - MIN_INTERVAL + 1) + MIN_INTERVAL);
         usleep(interval * 1000); // Arrival of the next customer
     }
@@ -70,6 +81,7 @@ int main()
     // End of the barber thread
     pthread_cancel(barber_thread);
     pthread_join(barber_thread, NULL);
+    pthread_cond_destroy(&barber_cond);
 
     printf("----------------------\n");
     printf("Customers served: %d\n", served_customers);
@@ -77,158 +89,109 @@ int main()
 
     return 0;
 }
-
 void *barber(void *arg)
 {
-    // Barber's work cycle
-    while (true)
+    while (1)
     {
-        // Synchronize the barber with the customers
         pthread_mutex_lock(&mutex);
-        // If there are no customers, the barber goes to sleep
-        if (waiting == 0)
+        while (customers_waiting == 0)
         {
-            is_barber_sleeping = true;
             barber_sleep();
-            // Waiting for the arrival of the customer
-            pthread_cond_wait(&customer_waiting, &mutex);
+            pthread_cond_wait(&barber_cond, &mutex);
         }
-        is_barber_sleeping = false;
 
-        // The barber is ready to serve the customer
-        pthread_cond_signal(&barber_available);
-        // The barber is cutting hair
+        entry *customer = TAILQ_FIRST(&head);
+        TAILQ_REMOVE(&head, customer, entries);
+        customers_waiting--;
+
+        barber_service(customer->id);
+
+        pthread_cond_signal(&customer->cond); // Notify the customer haircut is done
+
         pthread_mutex_unlock(&mutex);
-
-        barber_service();
-        barber_cutting++;
     }
-
     return NULL;
 }
+
 void *customer(void *arg)
 {
-    int number = (int)(long)arg;
+    struct entry *my_entry = malloc(sizeof(struct entry));
+    pthread_cond_init(&my_entry->cond, NULL);
+    my_entry->id = *(int *)arg;
+
+    customer_entry(my_entry->id);
 
     pthread_mutex_lock(&mutex);
-    customer_entry(number);
-    // If there is a free chair, the customer sits down
-    if (waiting < CHAIRS)
-    {
-        waiting++;
-        // if the barber is sleeping, wake him up
-        if (is_barber_sleeping)
-        {
-            pthread_cond_signal(&customer_waiting);
-            is_barber_sleeping = false;
-        }
-        //
-        pthread_mutex_unlock(&mutex);
-        customer_wait(number);
-        pthread_mutex_lock(&mutex);
-        // The customer is waiting for the barber to be ready
-        pthread_cond_wait(&barber_available, &mutex);
+    if (customers_waiting < 5)
+    { // Only 5 chairs available
+        customers_waiting++;
+        TAILQ_INSERT_TAIL(&head, my_entry, entries);
 
-        // The barber is ready to serve the customer
-        customer_service(number);
-        served_customers++;
-        // The customer leaves the barbershop
-        waiting--;
-        pthread_cond_signal(&customer_waiting);
-        pthread_mutex_unlock(&mutex);
+        customer_wait(my_entry->id);
 
-        customer_exit(number);
+        pthread_cond_signal(&barber_cond);          // Wake barber if sleeping
+        pthread_cond_wait(&my_entry->cond, &mutex); // Wait for the haircut to be done
+
+        customer_served(my_entry->id);
     }
     else
     {
-        // If there are no free chairs, the customer leaves the barbershop
-        rejected_customers++;
-        customer_reject(number);
-        pthread_mutex_unlock(&mutex);
+        customer_reject(my_entry->id);
     }
 
+    customer_exit(my_entry->id);
+    pthread_mutex_unlock(&mutex);
+    pthread_cond_destroy(&my_entry->cond);
     return NULL;
 }
 
 // Functions for printing messages barber sleep
 void barber_sleep()
 {
-
-    if (print)
-    {
-        printf("Barber sleep.\n");
-        fflush(stdout);
-    }
+    printf("Barber sleep.\n");
+    fflush(stdout);
 }
 
 // Function for printing messages barber service
-void barber_service()
+void barber_service(int customer_number)
 {
-    if (print)
-    {
-        printf("Barber cutting hair.\n");
-        fflush(stdout);
-    }
+    printf("Barber cutting hair of customer %d.\n", customer_number);
+    fflush(stdout);
+
     usleep(SERVICE_TIME * 1000);
-    if (print)
-    {
-        printf("Barber finished cutting hair.\n");
-        fflush(stdout);
-    }
+
+    printf("Barber finished cutting hair.\n");
+    fflush(stdout);
 }
 
 // Functions for printing messages customer entry
 void customer_entry(int number)
 {
-
-    if (print)
-    {
-        printf("Customer %d entered.\n", number);
-        fflush(stdout);
-    }
+    printf("Customer %d entered.\n", number);
+    fflush(stdout);
 }
 
 // Functions for printing messages customer wait
 void customer_wait(int number)
 {
-
-    if (print)
-    {
-        printf("Customer %d waiting.\n", number);
-        fflush(stdout);
-    }
+    printf("Customer %d waiting.\n", number);
 }
 
 // Functions for printing messages customer service
-void customer_service(int number)
+void customer_served(int number)
 {
-    if (print)
-    {
-        printf("Customer %d served.\n", number);
-        fflush(stdout);
-    }
-    usleep(SERVICE_TIME * 1000); // Simulace obsluhy zákazníka
-    if (print)
-    {
-        printf("Customer %d finished.\n", number);
-        fflush(stdout);
-    }
+    printf("Customer %d served.\n", number);
+    fflush(stdout);
 }
 
 void customer_exit(int number)
 {
-    if (print)
-    {
-        printf("Customer %d exited.\n", number);
-        fflush(stdout);
-    }
+    printf("Customer %d exited.\n", number);
+    fflush(stdout);
 }
 
 void customer_reject(int number)
 {
-    if (print)
-    {
-        printf("Customer %d rejected.\n", number);
-        fflush(stdout);
-    }
+    printf("Customer %d rejected.\n", number);
+    fflush(stdout);
 }
